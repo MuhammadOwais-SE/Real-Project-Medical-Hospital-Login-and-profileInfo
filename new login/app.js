da4bcddbc688b3005bb13d5513deb0a05f093032
unknown <owaisilyas22@gmail.com>
2025-12-31 11:15:28 +0500

    Saving the code. Add the timer for the loader to appear. Build the profolio

--- new login/app.js
+++ new login/app.js
@@ -1,5 +1,25 @@
 var loader = document.getElementById("loader");
 
-window.addEventListener("load", () =>{
-    loader.style.display = "none"
-})
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 1500);
+});
+
+const loginForm = document.querySelector('form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    loader.style.display = "block"; // Show loader
+
+    setTimeout(() => {
+        if (username === 'admin' && password === '123') {
+            window.location.href = '../UserInfo/index.html';
+        } else {
+            loader.style.display = "none"; // Hide loader on error
+            alert('Invalid Credentials');
+        }
+    }, 2000);
+});
